d55c1b40ccd9fe45075752ba81e7b0a8e2f16453
Mehdi660 <mehdi.belgoumrii@gmail.com>
2023-03-22 16:06:17 +0100

    ajout des fonctions en js pour filtrer les differents projets

--- Portfolio-architecte-sophie-bluel/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/index.js
@@ -1,10 +1,13 @@
 const gallery = document.querySelector(".gallery")
+// let category = window.localStorage.getItem('categoryId')
+let elementTableau;
 
 
 async function getWork() {
     try {
         const response = await fetch("http://localhost:5678/api/works")
         const json = await response.json()
+        elementTableau = json
         console.log(json);
         ajoutGallerie(json)
     } catch (error) {
@@ -15,13 +18,51 @@ getWork()
 
 function ajoutGallerie(json) {
     json.forEach(element => {
-        console.log(element.imageUrl, element.title);
+        // console.log(element.imageUrl, element.title);
         const figure = `<figure>
 				<img src=${element.imageUrl} alt=${element.title}>
 				<figcaption>${element.title}</figcaption>
 			</figure>`
         const gallery = document.querySelector(".gallery")
         gallery.innerHTML = figure + gallery.innerHTML
-        console.log(gallery.innerHTML);
+        // console.log(gallery.innerHTML);
+
     });
 }
+
+const btnAll = document.querySelector(".all");
+const btnObject = document.querySelector(".object");
+const btnAppart = document.querySelector(".appart");
+const btnHotels = document.querySelector(".hotel_and_restaurant");
+
+
+
+btnAll.addEventListener("click", () => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId)
+    console.log(projectFiltrees);
+    document.querySelector(".gallery").innerHTML = "";
+    ajoutGallerie(projectFiltrees)
+});
+
+btnObject.addEventListener("click", () => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId === 1)
+    console.log(projectFiltrees);
+    document.querySelector(".gallery").innerHTML = "";
+    ajoutGallerie(projectFiltrees)
+});
+
+btnAppart.addEventListener("click", () => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId === 2)
+    console.log(projectFiltrees);
+    document.querySelector(".gallery").innerHTML = "";
+    ajoutGallerie(projectFiltrees)
+});
+
+btnHotels.addEventListener("click", () => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId === 3)
+    console.log(projectFiltrees);
+    document.querySelector(".gallery").innerHTML = "";
+    ajoutGallerie(projectFiltrees)
+});
+
+
