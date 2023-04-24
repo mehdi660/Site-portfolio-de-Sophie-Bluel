366d4dee9e3a571c6915529c68ed5a7e65cf42d8
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-04-24 16:01:31 +0200

    refactorisation du projet

--- Portfolio-architecte-sophie-bluel/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/index.js
@@ -1,30 +1,29 @@
-const gallery = document.querySelector(".gallery")
-let category = window.localStorage.getItem('categoryId')
+const gallery = document.querySelector(".gallery");
+let category = window.localStorage.getItem('categoryId');
 let elementTableau;
-
 
 async function getWork() {
     try {
-        const response = await fetch("http://localhost:5678/api/works")
-        const json = await response.json()
-        elementTableau = json
-        ajoutGallerie(json)
+        const response = await fetch("http://localhost:5678/api/works");
+        const json = await response.json();
+        elementTableau = json;
+        ajoutGallerie(json);
     } catch (error) {
         console.log(error);
     }
 }
 
-getWork()
+getWork();
 
 function ajoutGallerie(json) {
     console.log(json);
     json.forEach(element => {
         const figure = `<figure>
-				<img src=${element.imageUrl} alt=${element.title}>
-				<figcaption>${element.title}</figcaption>
-			</figure>`
-        const gallery = document.querySelector(".gallery")
-        gallery.innerHTML = figure + gallery.innerHTML
+			<img src=${element.imageUrl} alt=${element.title}>
+			<figcaption>${element.title}</figcaption>
+		</figure>`;
+        const gallery = document.querySelector(".gallery");
+        gallery.innerHTML = figure + gallery.innerHTML;
     });
 }
 
@@ -33,94 +32,82 @@ const btnObject = document.querySelector(".object");
 const btnAppart = document.querySelector(".appart");
 const btnHotels = document.querySelector(".hotel_and_restaurant");
 
-
-// TODO Factoriser et mettre dans une fonction 
-btnAll.addEventListener("click", () => {
-    const projectFiltrees = elementTableau.filter(el => el.categoryId)
+// Fonction générique pour filtrer et mettre à jour la galerie
+const filtrerEtMettreAJourGalerie = (categoryId) => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId === categoryId);
     console.log(projectFiltrees);
     document.querySelector(".gallery").innerHTML = "";
-    ajoutGallerie(projectFiltrees)
+    ajoutGallerie(projectFiltrees);
+};
+
+// Écouteurs d'événements pour les boutons
+btnAll.addEventListener("click", () => {
+    const projectFiltrees = elementTableau.filter(el => el.categoryId);
+    document.querySelector(".gallery").innerHTML = "";
+    ajoutGallerie(projectFiltrees);
 });
 
-
 btnObject.addEventListener("click", () => {
-    const projectFiltrees = elementTableau.filter(el => el.categoryId === 1)
-    console.log(projectFiltrees);
-    document.querySelector(".gallery").innerHTML = "";
-    ajoutGallerie(projectFiltrees)
+    filtrerEtMettreAJourGalerie(1);
 });
 
 btnAppart.addEventListener("click", () => {
-    const projectFiltrees = elementTableau.filter(el => el.categoryId === 2)
-    console.log(projectFiltrees);
-    document.querySelector(".gallery").innerHTML = "";
-    ajoutGallerie(projectFiltrees)
+    filtrerEtMettreAJourGalerie(2);
 });
-
 
 btnHotels.addEventListener("click", () => {
-    const projectFiltrees = elementTableau.filter(el => el.categoryId === 3)
-    console.log(projectFiltrees);
-    document.querySelector(".gallery").innerHTML = "";
-    ajoutGallerie(projectFiltrees)
+    filtrerEtMettreAJourGalerie(3);
 });
 
-
-
-
-
-
 const token = localStorage.token;
-const editPart = document.querySelectorAll('.edit')
-const editTop = document.querySelector('.edit-top')
-const btnFilter = document.querySelector('.btn-filter')
-const login = document.querySelector('#login')
+const editPart = document.querySelectorAll('.edit');
+const editTop = document.querySelector('.edit-top');
+const btnFilter = document.querySelector('.btn-filter');
+const login = document.querySelector('#login');
 if (token) {
     editPart.forEach(editPart => {
-        editPart.style = "display: flex;"
-        editTop.style = "display: block;"
-    })
-    btnFilter.style = "display:none;"
-    login.innerHTML = "<li>logout</li>"
-
+        editPart.style = "display: flex;";
+        editTop.style = "display: block;";
+    });
+    btnFilter.style = "display:none;";
+    login.innerHTML = "<li>logout</li>";
 }
 
 login.addEventListener("click", () => {
     if (token) {
-        localStorage.removeItem('token')
+        localStorage.removeItem('token');
     }
 });
-
 
 const editBtn = document.querySelector('.edit-project');
 const modale = document.querySelector('.modale');
 const overlay = document.querySelector('.overlay');
-const cross = document.querySelector('.cross')
-const modaleAdd = document.querySelector('.modale-add')
-const closeMod = document.querySelector('.cross-add')
+const cross = document.querySelector('.cross');
+const modaleAdd = document.querySelector('.modale-add');
+const closeMod = document.querySelector('.cross-add');
 
 overlay.addEventListener("click", () => {
     if (overlay) {
-        modale.style = "display: none;"
-        overlay.style = "display: none;"
-        modaleAdd.style = "display: none;"
+        modale.style = "display: none;";
+        overlay.style = "display: none;";
+        modaleAdd.style = "display: none;";
     }
-})
+});
 
 cross.addEventListener("click", () => {
     if (cross) {
-        modale.style = "display: none;"
-        overlay.style = "display: none;"
+        modale.style = "display: none;";
+        overlay.style = "display: none;";
     }
-})
+});
 
 closeMod.addEventListener("click", () => {
     if (closeMod) {
-        modale.style = "display: none;"
-        overlay.style = "display: none;"
-        modaleAdd.style = "display: none;"
+        modale.style = "display: none;";
+        overlay.style = "display: none;";
+        modaleAdd.style = "display: none;";
     }
-})
+});
 
 
 function ajoutGallerieModale(json) {
@@ -282,26 +269,6 @@ addImg()
 
 
 
-
-// function btnTransition(){
-//     const allBtn = document.querySelectorAll(".btn");
-//     allBtn.forEach(item => {
-//         item.addEventListener("click", () => {
-//             const elemId = item.id.replace("btn_","");
-//             console.log(elemId);
-//             let projectFiltrees;
-//             if(elemId!=""){
-//                  projectFiltrees = elementTableau.filter(el => el.categoryId == elemId)
-//             }else{
-//                  projectFiltrees = elementTableau.filter(el => el.categoryId)
-//             }
-//             document.querySelector(".gallery").innerHTML = "";
-//             ajoutGallerie(projectFiltrees)
-//         });
-//     });
-// }
-
-// btnTransition();
 
 
 
